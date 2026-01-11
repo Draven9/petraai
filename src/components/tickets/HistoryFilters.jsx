9755fdf71d1b430aacd65cad059958ecc279caa8
Draven9 <eric.azevedo9@gmail.com>
2026-01-10 21:02:35 -0300

    Histórico de Filtros

--- src/components/tickets/HistoryFilters.jsx
+++ src/components/tickets/HistoryFilters.jsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button"
+import { Badge } from "@/components/ui/badge"
 import { Calendar } from "@/components/ui/calendar"
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
